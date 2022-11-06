d79aab5dca5357b1abd956c6a7287dca138ab0ac
lbh <bongho69@gmail.com>
2022-11-06 23:07:55 +0900

    Second Commit
    - Test

---
+++ src/main/java/com/example/jdbcexample/application/config/JdbcConfig.java
@@ -0,0 +1,8 @@
+package com.example.jdbcexample.application.config;
+
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+
+@EnableJdbcRepositories
+public class JdbcConfig {
+
+}
